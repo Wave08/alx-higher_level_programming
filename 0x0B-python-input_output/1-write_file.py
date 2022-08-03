2366fb8c952f18cdb0987f573fe25af3fa1dc8b5
Wave08 <mararakudakwashe@gmail.com>
2022-08-03 07:39:00 -0700

    task1done

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,7 +6,16 @@ returns the number of characters written
 
 
 def write_file(filename="", text=""):
-    """ module write_file
-    """
-    with open(filename, 'w') as f:
-        return f.write(text)
+    '''Writes a UTF-8 encoded text to a file
+
+    Args:
+    filename (str): The name of the file to write to
+    text (str): The content to store in the file
+
+    Returns:
+    int: The number of characters written
+    '''
+    n = 0
+    with open(filename, mode='w', encoding='utf-8') as file:
+        n = file.write(text)
+        return n
