dea6c88d42d25944d1c5d6f3472a874d259f45c1
Wave08 <mararakudakwashe@gmail.com>
2022-09-08 15:54:15 -0700

    code corrected

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,2 @@
--- this script add a new row in table first_table id = 89 , and name is Best School
-INSERT INTO first_table (id INT, name)
-VALUES (89, Best School);
+-- this script add a new row in table first_table id = 89 TO first_table
+INSERT INTO first_table VALUES (89, 'Best School');
