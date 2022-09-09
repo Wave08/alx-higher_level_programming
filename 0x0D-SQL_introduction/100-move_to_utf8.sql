0adb25a42dc2c4aca3e8b22546c02998f25a205d
Wave08 <mararakudakwashe@gmail.com>
2022-09-09 01:35:49 -0700

    task correccted

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,4 @@
 -- this script converts the entire database to UTF*
-USE 'hbtn_0c_0'
-ALTER TABLE 'first_table'
+USE `hbtn_0c_0`
+ALTER TABLE `first_table`
 CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
