0dbba96e24c70b9ae141d0e23e09a043a2ba35d3
Wave08 <99523632+Wave08@users.noreply.github.com>
2022-10-07 14:21:53 +0200

    task done

--- 0x12-javascript_warm_up/2-arguments.js
+++ 0x12-javascript_warm_up/2-arguments.js
@@ -1,1 +1,8 @@
-#!/usr/binnode
+#!/usr/bin/node
+const argc = process.argv.length;
+
+if (argc > 2) {
+  console.log('Argument' + (argc > 3 ? 's' : '') + ' found');
+} else {
+  console.log('No argument');
+}
